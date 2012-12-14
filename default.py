e44fc6cf9c82faff23586bb53e1786a1e2f01ea1
Thomas Amland <thomas.amland@gmail.com>
2012-12-14 19:42:44 +0100

    added radio channels to live streams

--- default.py
+++ default.py
@@ -43,15 +43,30 @@ def view_top():
 @plugin.route('/live')
 def live():
   b = ['380','659','1394','2410','3660'][BITRATE-1]
-  img_path = os.path.join(ADDON_PATH, "resources/images")
-  add("NRK 1", "http://nrk1us-f.akamaihd.net/i/nrk1us_0@79328/index_%s_av-p.m3u8?sd=10&rebase=on" % b,os.path.join(img_path, "nrk1.png"))
-  add("NRK 2", "http://nrk2us-f.akamaihd.net/i/nrk2us_0@79327/index_%s_av-p.m3u8?sd=10&rebase=on" % b, os.path.join(img_path, "nrk2.png"))
-  add("NRK 3", "http://nrk3us-f.akamaihd.net/i/nrk3us_0@79326/index_%s_av-p.m3u8?sd=10&rebase=on" % b, os.path.join(img_path, "nrk3.png"))
+  add("NRK 1", "http://nrk1us-f.akamaihd.net/i/nrk1us_0@79328/index_%s_av-p.m3u8?sd=10&rebase=on" % b, "application/vnd.apple.mpegurl", "nrk1.png")
+  add("NRK 2", "http://nrk2us-f.akamaihd.net/i/nrk2us_0@79327/index_%s_av-p.m3u8?sd=10&rebase=on" % b, "application/vnd.apple.mpegurl", "nrk2.png")
+  add("NRK 3", "http://nrk3us-f.akamaihd.net/i/nrk3us_0@79326/index_%s_av-p.m3u8?sd=10&rebase=on" % b, "application/vnd.apple.mpegurl", "nrk3.png")
+  add("NRK P1", "http://lyd.nrk.no/nrk_radio_p1_ostlandssendingen_mp3_h", "audio/mpeg")
+  add("NRK P2", "http://lyd.nrk.no/nrk_radio_p2_mp3_h", "audio/mpeg")
+  add("NRK P3", "http://lyd.nrk.no/nrk_radio_p3_mp3_h", "audio/mpeg")
+  add("Alltid nyheter", "http://lyd.nrk.no/nrk_radio_alltid_nyheter_mp3_h", "audio/mpeg")
+  add("Jazz", "http://lyd.nrk.no/nrk_radio_jazz_mp3_h", "audio/mpeg")
+  add("Klassisk", "http://lyd.nrk.no/nrk_radio_klassisk_mp3_h", "audio/mpeg")
+  add("Folkemusikk", "http://lyd.nrk.no/nrk_radio_folkemusikk_mp3_h", "audio/mpeg")
+  add("Gull", "http://lyd.nrk.no/nrk_radio_gull_mp3_h", "audio/mpeg")
+  add("mP3", "http://lyd.nrk.no/nrk_radio_mp3_mp3_h", "audio/mpeg")
+  add("P3 Urørt", "http://lyd.nrk.no/nrk_radio_p3_urort_mp3_h", "audio/mpeg")
+  add("Sport", "http://lyd.nrk.no/nrk_radio_sport_mp3_h", "audio/mpeg")
+  add("Sápmi", "http://lyd.nrk.no/nrk_radio_sami_mp3_h", "audio/mpeg")
+  add("Super", "http://lyd.nrk.no/nrk_radio_super_mp3_h", "audio/mpeg")
   endOfDirectory(plugin.handle)
 
-def add(title, url, thumb=""):
+def add(title, url, mimetype, thumb=""):
+  if thumb:
+    img_path = os.path.join(ADDON_PATH, "resources/images")
+    thumb = os.path.join(img_path, thumb)
   li =  ListItem(title, thumbnailImage=thumb)
-  li.setProperty('mimetype', 'application/vnd.apple.mpegurl')
+  li.setProperty('mimetype', mimetype)
   addDirectoryItem(plugin.handle, url, li, False)
 
 
